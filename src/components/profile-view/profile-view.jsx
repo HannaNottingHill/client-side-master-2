41a038e8a797d47867f20c93ebe93af387b6ab8c
HannaNottingHill <hanacajkas@icloud.com>
2023-11-15 16:24:28 +0000

    update

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -13,6 +13,21 @@ function ProfileView({
   );
   const [userData, setUserData] = useState(null);
 
+  // Initialize state variables for form fields
+  const [editUsername, setEditUsername] = useState("");
+  const [editPassword, setEditPassword] = useState("");
+  const [editEmail, setEditEmail] = useState("");
+  const [editBirthday, setEditBirthday] = useState("");
+
+  // Populate form fields with user data when available
+  useEffect(() => {
+    if (userData) {
+      setEditUsername(userData.username);
+      setEditEmail(userData.email);
+      setEditBirthday(userData.birthday);
+    }
+  }, [userData]);
+
   useEffect(() => {
     // Fetch user data from the /users endpoint
     if (user) {
@@ -30,6 +45,29 @@ function ProfileView({
         });
     }
   }, [user]);
+
+  const handleProfileUpdate = (e) => {
+    e.preventDefault(); // Prevent default form submission
+    fetch(`http://localhost:8080/users/${user.username}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+      body: JSON.stringify({
+        username: editUsername,
+        password: editPassword, // Handle empty password appropriately
+        email: editEmail,
+        birthday: editBirthday,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Update user data and potentially update local storage or user state
+        setUserData(data);
+      })
+      .catch((error) => console.error("Error updating profile: ", error));
+  };
 
   return (
     <div>
@@ -61,6 +99,30 @@ function ProfileView({
       ) : (
         <p>No favorites added.</p>
       )}
+
+      <form onSubmit={handleProfileUpdate}>
+        <input
+          type="text"
+          value={editUsername}
+          onChange={(e) => setEditUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="New Password (leave blank to keep current)"
+          onChange={(e) => setEditPassword(e.target.value)}
+        />
+        <input
+          type="email"
+          value={editEmail}
+          onChange={(e) => setEditEmail(e.target.value)}
+        />
+        <input
+          type="date"
+          value={editBirthday}
+          onChange={(e) => setEditBirthday(e.target.value)}
+        />
+        <button type="submit">Update Profile</button>
+      </form>
     </div>
   );
 }
